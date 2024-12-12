ca40ab4ad2feda0bcb5cf990827b6b89919d3cf4
Isaac <isaac@codegalaxy.co.uk>
2024-12-12 16:51:56 +0000

    hmmm

--- packages/hooks/user.ts
+++ packages/hooks/user.ts
@@ -32,33 +32,17 @@ import { useKarrio, useKarrioLogin, setupRestClient } from "./karrio";
 
 export function useUser() {
   const karrio = useKarrioLogin();
-
-  console.log('useUser: karrio context:', karrio);
-
   const user = karrio.pageData?.user;
 
   // Queries
   const query = useQuery({
     queryKey: ["user"],
-    queryFn: async () => {
-      console.log('useUser: Making GraphQL request with karrio.graphql:', karrio.graphql);
-      if (!karrio.graphql) {
-        throw new Error("GraphQL client is not initialized");
-      }
-
-      console.log('some shit about user', GET_USER)
-
-      const response = await karrio.graphql.request<GetUser>(gqlstr(GET_USER));
-      console.log('useUser: GraphQL response:', response);
-      return response;
-    },
-    initialData: user ? { user } : undefined,
+    queryFn: () => karrio.graphql.request<GetUser>(gqlstr(GET_USER)),
+    initialData: !!user ? { user } : undefined,
     refetchOnWindowFocus: false,
     staleTime: 300000,
-    enabled: !!karrio.graphql,
-    onError: (error: Error) => {
-      console.error("Failed to fetch user data:", error);
-    },
+    enabled: !!user,
+    onError,
   });
 
   return {
